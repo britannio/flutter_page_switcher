b208726621a0dcf20103516ad72ed056c870585e
Britannio <britanniojarrett@gmail.com>
2020-03-24 19:54:06 +0000

    add page switcher and test

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,3 @@
-## [0.0.1] - TODO: Add release date.
+## [1.0.0] - 24/3/20
 
-* TODO: Describe initial release.
+* Initial release
--- test/flutter_page_switcher_test.dart
+++ test/flutter_page_switcher_test.dart
@@ -1,13 +1,44 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_page_switcher/flutter_page_switcher.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:flutter_page_switcher/flutter_page_switcher.dart';
-
 void main() {
-  test('adds one to input values', () {
-    final calculator = Calculator();
-    expect(calculator.addOne(2), 3);
-    expect(calculator.addOne(-7), -6);
-    expect(calculator.addOne(0), 1);
-    expect(() => calculator.addOne(null), throwsNoSuchMethodError);
+  testWidgets('flutter_page_switcher.dart | PageSwitcher behaves as intended', (
+    WidgetTester tester,
+  ) async {
+    const List<Widget> children = <Widget>[Text('1'), Text('2')];
+    final StreamController<int> _indexController = StreamController<int>();
+
+    final Widget pageSwitcher = StreamBuilder<int>(
+      stream: _indexController.stream,
+      initialData: 1,
+      builder: (_, AsyncSnapshot<int> snapshot) {
+        return PageSwitcher(pageIndex: snapshot.data, children: children);
+      },
+    );
+
+    // Displays two pages, starting at page 2
+    await tester.pumpWidget(testableWidget(child: pageSwitcher));
+
+    // Used to identify the page being shown
+    final Finder pageOneFinder = find.text('1');
+    final Finder pageTwoFinder = find.text('2');
+    // Checks if the correct page is being shown
+    expect(pageOneFinder, findsNothing);
+    expect(pageTwoFinder, findsOneWidget);
+    // Switches to the first page
+    _indexController.add(0);
+    // Rebuilds the widget
+    await tester.pump();
+    // Checks if the correct page is being shown
+    expect(pageOneFinder, findsOneWidget);
+    expect(pageTwoFinder, findsNothing);
+
+    _indexController.close();
   });
 }
+
+// Mimics an actual app to enable widgets to operate correctly
+Widget testableWidget({@required Widget child}) => MaterialApp(home: child);
